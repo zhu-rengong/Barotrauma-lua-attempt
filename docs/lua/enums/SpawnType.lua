907061ec0063e4ff56344b14ad79bab36d41ad00
Evil Factory <36804725+evilfactory@users.noreply.github.com>
2021-12-07 17:55:47 -0300

    add SpawnType enum

---
+++ docs/lua/enums/SpawnType.lua
@@ -0,0 +1,29 @@
+--[[--
+SpawnType enum.
+]]
+-- @enum SpawnType
+
+---
+-- SpawnType.Path = 0
+-- @realm shared
+-- @number SpawnType.Path
+
+---
+-- SpawnType.Human = 1
+-- @realm shared
+-- @number SpawnType.Human
+
+---
+-- SpawnType.Enemy = 2
+-- @realm shared
+-- @number SpawnType.Enemy
+
+---
+-- SpawnType.Cargo = 4
+-- @realm shared
+-- @number SpawnType.Cargo
+
+---
+-- SpawnType.Corpse = 8
+-- @realm shared
+-- @number SpawnType.Corpse
