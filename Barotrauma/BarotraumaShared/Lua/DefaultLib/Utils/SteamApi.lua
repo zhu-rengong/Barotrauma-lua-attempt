ca0da04ad017062444182a46ba52af5acc2f93d3
Evil Factory <36804725+evilfactory@users.noreply.github.com>
2022-05-19 14:26:21 -0300

    better LuaUserData error handling and add Steam.GetWorkshopCollection

---
+++ Barotrauma/BarotraumaShared/Lua/DefaultLib/Utils/SteamApi.lua
@@ -0,0 +1,26 @@
+local descriptor = LuaUserData.RegisterType("Barotrauma.LuaCsSteam")
+
+LuaUserData.AddMethod(descriptor, "GetWorkshopCollection", function (id, callback)
+    id = tostring(id)
+
+    Networking.RequestPostHTTP("https://api.steampowered.com/ISteamRemoteStorage/GetCollectionDetails/v1/", function (result)
+        local data = json.parse(result)
+
+        if data.response.collectiondetails.children == nil then
+            callback()
+            return
+        end
+
+        local workshopItems = {}
+
+        for key, value in pairs(data.response.collectiondetails[1].children) do
+            table.insert(workshopItems, value.publishedfileid)
+        end
+
+        if callback then
+            callback(workshopItems)
+        end
+    end,
+    "collectioncount=1&publishedfileids[0]=" .. id,
+    "application/x-www-form-urlencoded")
+end)
