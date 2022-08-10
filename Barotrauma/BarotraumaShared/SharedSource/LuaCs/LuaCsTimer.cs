40814a17237b46eafbb9a3b5ca4fc7a2ddeb46f1
EvilFactory <evilfactory@users.noreply.github.com>
2022-08-10 16:35:12 -0300

    expose AccumulatorMax

--- Barotrauma/BarotraumaShared/SharedSource/LuaCs/LuaCsTimer.cs
+++ Barotrauma/BarotraumaShared/SharedSource/LuaCs/LuaCsTimer.cs
@@ -8,6 +8,17 @@ namespace Barotrauma
     {
         public static double Time => Timing.TotalTime;
         public static double GetTime() => Time;
+        public static double AccumulatorMax
+        {
+            get
+            {
+                return Timing.AccumulatorMax;
+            }
+            set
+            {
+                Timing.AccumulatorMax = value;
+            }
+        }
 
         private class TimerComparer : IComparer<TimedAction>
         {
