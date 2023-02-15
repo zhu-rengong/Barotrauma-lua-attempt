757998a1e4de73054b4df6dd7d9ef67fafbc227d
EvilFactory <evilfactory@users.noreply.github.com>
2023-02-15 19:24:54 -0300

    Improved docs: Members now have their realm set automatically and server-only members are now also included

--- luacs-docs/lua/baseluadocs/Character.lua
+++ luacs-docs/lua/baseluadocs/Character.lua
@@ -10,33 +10,8 @@ Barotrauma source code: [Character.cs](https://github.com/evilfactory/LuaCsForBa
 
 local Character = {}
 
--- @remove function Character.Create(characterInfo, position, seed, id, isRemotePlayer, hasAi, ragdoll) end
--- @remove function TeleportTo(worldPos) end
 -- @remove Character.CharacterList
 
---- Creates a Character using CharacterInfo.
--- @realm server
--- @tparam CharacterInfo characterInfo
--- @tparam Vector2 position
--- @tparam string seed
--- @tparam number id
--- @tparam bool isRemotePlayer
--- @tparam bool hasAi
--- @tparam RagdollParams ragdoll
--- @treturn Character
--- @usage 
--- local vsauce = CharacterInfo("human", "custom name")
--- local character = Character.Create(vsauce, Vector2(0, 0), "some random characters")
--- print(character)
-function Character.Create(characterInfo, position, seed, id, isRemotePlayer, hasAi, ragdoll) end
-
-
---- Teleports a character to a position.
--- @realm server 
--- @tparam Vector2 worldPos
--- @usage 
--- Character.CharacterList[1].TeleportTo(Vector2(0, 0)) -- teleports first created characters to 0, 0
-function TeleportTo(worldPos) end
 
 ---
 -- Character.CharacterList, Table containing all characters.
