966236652a2a038f18d1ba7a72a8e2089463f9cd
EvilFactory <evilfactory@users.noreply.github.com>
2023-10-08 17:49:46 -0300

    Copy publicized folder on install script and clean up

--- Barotrauma/BarotraumaShared/SharedSource/LuaCs/LuaCsInstaller.cs
+++ Barotrauma/BarotraumaShared/SharedSource/LuaCs/LuaCsInstaller.cs
@@ -7,11 +7,32 @@ namespace Barotrauma
 {
     static partial class LuaCsInstaller
     {
+        private static string[] trackingFiles = new string[]
+        {
+            "Barotrauma.dll", "Barotrauma.deps.json", "Barotrauma.pdb",
+            "0Harmony.dll", "Mono.Cecil.dll",
+            "Sigil.dll",
+            "Mono.Cecil.Mdb.dll", "Mono.Cecil.Pdb.dll",
+            "Mono.Cecil.Rocks.dll", "MonoMod.Common.dll",
+            "MoonSharp.Interpreter.dll", "MoonSharp.VsCodeDebugger.dll",
+
+            "Microsoft.CodeAnalysis.dll", "Microsoft.CodeAnalysis.CSharp.dll",
+            "Microsoft.CodeAnalysis.CSharp.Scripting.dll", "Microsoft.CodeAnalysis.Scripting.dll",
+
+            "System.Reflection.Metadata.dll", "System.Collections.Immutable.dll",
+            "System.Runtime.CompilerServices.Unsafe.dll",
+
+            "Publicized/DedicatedServer.dll", "Publicized/Barotrauma.dll"
+        };
+
         private static void CreateMissingDirectory()
         {
             Directory.CreateDirectory("Temp/Original");
             Directory.CreateDirectory("Temp/ToDelete");
+            Directory.CreateDirectory("Temp/ToDelete/Publicized");
             Directory.CreateDirectory("Temp/Old");
+            Directory.CreateDirectory("Temp/Old/Publicized");
+            Directory.CreateDirectory("Publicized");
         }
 
     }
